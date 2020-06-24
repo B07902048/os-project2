dc5a535c1a96941f72cabd5f2d1b8f51a378ac4d
B07902048 <b07902048@ntu.edu.tw>
2020-06-24 02:47:00 -0700

    demo finish

--- master_device/.tmp_versions/master_device.mod
+++
@@ -1,3 +0,0 @@
-/home/anthony/os-project2/master_device/master_device.ko
-/home/anthony/os-project2/master_device/master_device.o
-
--- slave_device/.slave_device.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/anthony/os-project2/slave_device/slave_device.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/anthony/os-project2/slave_device/slave_device.ko /home/anthony/os-project2/slave_device/slave_device.o /home/anthony/os-project2/slave_device/slave_device.mod.o ;  true
